3f61ebeab8eae657eec055b757960d80f4d9774e
a.volodin <erik_sundberg@mail.ru>
2023-10-04 15:24:08 +0300

    Add some table into tasks

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -12,27 +12,35 @@ defineProps({
     <td class="header">
       <span>Задачи/даты</span>
     </td>
+    <td v-for="date in dates" :key="date.value" class="table-data">
+      <span>{{ date.value }}</span>
+    </td>
   </tr>
-  <tr class="list" v-for="item in tasks" :key="item">
-    <td class="list-item">
+  <tr class="list" v-for="item in tasks" :key="item.value">
+    <td class="table-data">
       <input type="text" v-model="item.value" />
+    </td>
+    <td v-for="date in dates" :key="date.value">
+      Список пуст
     </td>
   </tr>
 </table>
 </template>
 
 <style scoped>
-.tasks {
+table, tr, td {
   border: var(--border);
+}
+.tasks {
   .header {
     text-align: center;
     color: var(--text-secondary);
     border-bottom: 2px solid lightgray;
   }
   .list {
-    .list-item {
-      padding: 5px 40px;
-    }
+  }
+  .table-data {
+    padding: 5px 40px;
   }
 }
 </style>
