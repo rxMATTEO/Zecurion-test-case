156d084c69d00b9b4e208e0db512e018a425afce
a.volodin <erik_sundberg@mail.ru>
2023-10-04 15:34:58 +0300

    Add status switch

--- src/components/StatusSwitch.vue
+++ src/components/StatusSwitch.vue
@@ -1,11 +1,12 @@
 <script setup>
-import {computed} from "vue";
+import {computed, ref} from "vue";
 
 const props = defineProps({
   statuses: Array,
 });
 
 const isStatusesEmpty = computed(() => props.statuses.length === 0);
+const isClicked = ref(false);
 const emptyMessage = 'Список пуст';
 </script>
 
@@ -14,8 +15,15 @@ const emptyMessage = 'Список пуст';
     <div v-if="isStatusesEmpty">
       {{ emptyMessage }}
     </div>
-    <div v-else>
+    <div v-else-if="!isClicked" @click="isClicked = true">
       +
+    </div>
+    <div v-else>
+      <select>
+        <option v-for="status in statuses" :key="status">
+          {{ status.value }}
+        </option>
+      </select>
     </div>
   </div>
 </template>
